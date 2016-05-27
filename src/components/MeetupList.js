b7a82ee7e28b47d2762022b8da3a81e1ea1f6e41
Daniel Schmidt <dschmidt@weluse.de>
2016-05-27 23:07:02 +0200

    added meetup list

---
+++ src/components/MeetupList.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import {
+  ListView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+export default class MeetupList extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = this.getNewState(this.props.meetups);
+  }
+
+  getNewState(meetups = []) {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1.updated !== r2.updated
+    });
+
+    return {
+      dataSource: ds.cloneWithRows(meetups),
+    };
+  }
+
+  componentWillUpdate(nextProps) {
+    this.setState(this.getNewState(nextProps.meetups));
+  }
+
+  render() {
+    const {
+      dataSource,
+    } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <ListView
+          dataSource={dataSource}
+          renderRow={this.renderRow}
+        />
+      </View>
+    );
+  }
+
+  renderRow(rowData, sectionID, rowID, highlightRow) {
+    return (
+      <Text>{rowData}</Text>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+  },
+});
