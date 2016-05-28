1480be59d38b19d44adcc5582dc587513b1add6c
Daniel Schmidt <dschmidt@weluse.de>
2016-05-29 00:18:27 +0200

    added meetup component

--- src/App.js
+++ src/App.js
@@ -6,13 +6,36 @@ import {
 import Headline from './components/Headline';
 import MeetupList from './components/MeetupList';
 
+const meetups = [{
+  name: 'My fancy Meetup',
+  photo: 'http://photos2.meetupstatic.com/photos/event/5/b/c/3/highres_450383491.jpeg',
+  rsvpLimit: 25,
+  rsvps: 24,
+  time: 1464195600000,
+  venueName: 'My fancy location',
+}, {
+  name: 'Another great Meetup',
+  photo: 'http://photos4.meetupstatic.com/photos/event/5/c/7/d/highres_450383677.jpeg',
+  rsvpLimit: 50,
+  rsvps: 10,
+  time: 1464470131656,
+  venueName: 'Another great location',
+}, {
+  name: 'Let\'s meet again!',
+  photo: 'http://photos3.meetupstatic.com/photos/event/5/c/8/3/highres_450383683.jpeg',
+  rsvpLimit: 10,
+  rsvps: 8,
+  time: 1462572130000,
+  venueName: 'Another great location',
+}];
+
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.top}>
           <Headline text="React (Native) Kiel" />
-          <MeetupList meetups={['2. Meetup', '1. Meetup']} />
+          <MeetupList meetups={meetups} />
         </View>
       </View>
     );
@@ -30,5 +53,6 @@ const styles = StyleSheet.create({
   },
   top: {
     alignSelf: 'flex-start',
+    flex: 1,
   }
 });
