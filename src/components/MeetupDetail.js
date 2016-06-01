10a2f5570b94c9d9ff992b876e2b9d56226cb5a9
Daniel Schmidt <dschmidt@weluse.de>
2016-06-01 23:52:16 +0200

    Added Image Gallery with lightbox

--- src/components/MeetupDetail.js
+++ src/components/MeetupDetail.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
 import {
   Dimensions,
+  Image,
+  ListView,
   StyleSheet,
   View,
 } from 'react-native';
+import Lightbox from 'react-native-lightbox';
 
 import Headline from './Headline';
 import BackButton from './BackButton';
 
+const meetupPhotos = ['http://photos1.meetupstatic.com/photos/event/5/b/b/1/600_450383473.jpeg','http://photos2.meetupstatic.com/photos/event/5/c/7/d/600_450383677.jpeg','http://photos3.meetupstatic.com/photos/event/5/c/8/3/600_450383683.jpeg','http://photos2.meetupstatic.com/photos/event/5/c/d/c/600_450383772.jpeg'];
+
 export default class MeetupDetail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = this.getNewState(meetupPhotos);
+  }
+
+  getNewState(meetupPhotos = []) {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    return {
+      dataSource: ds.cloneWithRows(meetupPhotos),
+    };
+  }
+
   render() {
+    const {
+      dataSource,
+    } = this.state;
+
     const {
       name,
       navigator,
@@ -20,6 +44,19 @@ export default class MeetupDetail extends Component {
         <View stlye={styles.headline}>
           <Headline text={name} />
         </View>
+
+        <ListView
+          dataSource={dataSource}
+          renderRow={imageUrl => (
+            <Lightbox>
+              <Image
+                style={{ height: 300 }}
+                source={{ uri: imageUrl }}
+              />
+            </Lightbox>
+          )}
+        />
+
         <BackButton navigator={navigator} />
       </View>
     );
