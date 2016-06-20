65bcae6eb29dadab69a7cc3a6854ffbe2a03c1fd
Daniel Schmidt <dschmidt@weluse.de>
2016-06-20 23:12:51 +0200

    load meetups from API

--- src/App.js
+++ src/App.js
@@ -5,30 +5,7 @@ import {
 } from 'react-native';
 import Headline from './components/Headline';
 import MeetupList from './components/MeetupList';
-import { meetupName } from './services/meetup';
-
-const meetups = [{
-  name: 'My fancy Meetup',
-  photo: 'http://photos2.meetupstatic.com/photos/event/5/b/c/3/highres_450383491.jpeg',
-  rsvpLimit: 25,
-  rsvps: 24,
-  time: 1464195600000,
-  venueName: 'My fancy location',
-}, {
-  name: 'Another great Meetup',
-  photo: 'http://photos4.meetupstatic.com/photos/event/5/c/7/d/highres_450383677.jpeg',
-  rsvpLimit: 50,
-  rsvps: 10,
-  time: 1464470131656,
-  venueName: 'Another great location',
-}, {
-  name: 'Let\'s meet again!',
-  photo: 'http://photos3.meetupstatic.com/photos/event/5/c/8/3/highres_450383683.jpeg',
-  rsvpLimit: 10,
-  rsvps: 8,
-  time: 1462572130000,
-  venueName: 'Another great location',
-}];
+import { allEvents, meetupName } from './services/meetup';
 
 export default class App extends Component {
   constructor(props) {
@@ -42,6 +19,12 @@ export default class App extends Component {
         name,
       });
     });
+
+    allEvents().then(meetups => {
+      this.setState({
+        meetups,
+      });
+    })
   }
 
   render() {
@@ -51,6 +34,7 @@ export default class App extends Component {
 
     const {
       name,
+      meetups = [],
     } = this.state;
 
     return (
